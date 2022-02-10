034070d73d36962d1b2d335ac22fee366397d294
Sarah Westbrook <swestbrook1876@gmail.com>
2022-02-09 21:57:57 -0600

    Add files via upload

---
+++ initials.py
@@ -0,0 +1,37 @@
+#Sarah Westbrook
+#Fun Fact:I love music
+
+name_first = """
+  SSS
+S     S
+S
+ SSS 
+      S
+S     S
+  SSS 
+"""
+print(name_first)
+
+name_middle = """
+  CCC 
+C     C
+C
+C
+C
+C     C
+  CCC 
+"""
+print(name_middle)
+
+
+name_last = """
+W     W
+W     W
+W     W
+W  W  W
+WW W WW
+WW W WW
+W     W
+"""
+
+print(name_last)
